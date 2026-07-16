d1d82b39be7f4870770d0c54735b53ff7fd6f054
Peter Eck <jpeckiii@gmail.com>
2026-07-16 14:28:11 -0500

    Add repository method to fetch note by category ID, name, and createdBy

--- reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/NoteRepository.java
+++ reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/NoteRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoteRepository extends JpaRepository<NoteEntity, String> {
     List<NoteEntity> findByCategory(@NotNull NoteCategoryEntity category);
-    NoteEntity createNote(NoteEntity note);
+    Optional<NoteEntity> findByCategoryAndNameAndCreatedBy(NoteCategoryEntity category, String name, String createdBy);
 }
