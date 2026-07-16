fe76fd73658db13f8ceb451e4eef0824aa3622b2
Peter Eck <jpeckiii@gmail.com>
2026-07-16 14:28:35 -0500

    Add category ID endpoint

--- reviews-api/src/main/java/com/bourbon_nook/reviews_api/controllers/NoteController.java
+++ reviews-api/src/main/java/com/bourbon_nook/reviews_api/controllers/NoteController.java
@@ -1,10 +1,11 @@
 package com.bourbon_nook.reviews_api.controllers;
 
+import com.bourbon_nook.reviews_api.dtos.NoteDto;
+import com.bourbon_nook.reviews_api.mappers.NoteMapper;
 import com.bourbon_nook.reviews_api.models.responses.NoteResponseModel;
 import com.bourbon_nook.reviews_api.services.NoteService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -13,27 +14,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
-    Logger logger = LoggerFactory.getLogger(NoteController.class);
-
     private final Environment env;
     private final NoteService noteService;
+    private final NoteMapper noteMapper;
 
-    public NoteController(Environment env, NoteService noteService) {
+    public NoteController(Environment env, NoteService noteService, NoteMapper noteMapper) {
         this.env = env;
         this.noteService = noteService;
+        this.noteMapper = noteMapper;
     }
 
     @GetMapping("/status/healthcheck")
     public String healthcheck() { return "Notes: Working on port " + env.getProperty("local.server.port"); }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/{reviewId}")
-    public ResponseEntity<List<NoteResponseModel>> reviewNotes(@PathVariable String reviewId, Authentication authentication) {
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<List<NoteResponseModel>> categoryNotes(@PathVariable String categoryId, Authentication authentication) {
+        List<NoteDto> notesForCategory = noteService.getCategoryNotes(categoryId);
 
+        List<NoteResponseModel> responseModels = new ArrayList<>();
+        for (NoteDto noteDto : notesForCategory) {
+            responseModels.add(noteMapper.toResponseModel(noteDto));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(responseModels);
     }
 }
