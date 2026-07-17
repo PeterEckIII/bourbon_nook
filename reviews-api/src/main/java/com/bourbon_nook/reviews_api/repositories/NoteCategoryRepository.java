46d1c1017e29946450e1b138b9c3bcbc80afd221
Peter Eck <jpeckiii@gmail.com>
2026-07-17 16:30:53 -0500

    Make findByName return Optional

--- reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/NoteCategoryRepository.java
+++ reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/NoteCategoryRepository.java
@@ -1,11 +1,12 @@
 package com.bourbon_nook.reviews_api.repositories;
 
-import com.bourbon_nook.reviews_api.dtos.NoteCategoryDto;
 import com.bourbon_nook.reviews_api.entities.NoteCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NoteCategoryRepository extends JpaRepository<NoteCategoryEntity, String> {
-    NoteCategoryDto findByName(String name);
+    Optional<NoteCategoryEntity> findByName(String name);
 }
