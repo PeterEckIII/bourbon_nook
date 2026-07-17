8b0dd69f00cd1d2a2e59788b02bba2e099806271
Peter Eck <jpeckiii@gmail.com>
2026-07-17 14:05:30 -0500

    Make findByUserAndId return optional for error handling

--- reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/ReviewRepository.java
+++ reviews-api/src/main/java/com/bourbon_nook/reviews_api/repositories/ReviewRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, String> {
     List<ReviewEntity> findAllByUserId(String userId);
-    ReviewEntity findByIdAndUserId(String reviewId, String userId);
+    Optional<ReviewEntity> findByIdAndUserId(String reviewId, String userId);
 }
