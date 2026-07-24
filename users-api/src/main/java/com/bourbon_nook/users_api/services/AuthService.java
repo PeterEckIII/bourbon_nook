c8209e6b766cd39e7c3573ed3343fce1077a67d0
Peter Eck <jpeckiii@gmail.com>
2026-07-24 16:51:42 -0500

    Add AuthService to handle /auth endpoint logic

--- users-api/src/main/java/com/bourbon_nook/users_api/services/AuthService.java
+++ users-api/src/main/java/com/bourbon_nook/users_api/services/AuthService.java
@@ -1,4 +1,7 @@
 package com.bourbon_nook.users_api.services;
 
+import com.bourbon_nook.users_api.entities.UserEntity;
+
 public interface AuthService {
+    UserEntity getCurrentUser();
 }
--- users-api/src/main/java/com/bourbon_nook/users_api/services/AuthServiceImpl.java
+++ users-api/src/main/java/com/bourbon_nook/users_api/services/AuthServiceImpl.java
@@ -1,4 +1,27 @@
 package com.bourbon_nook.users_api.services;
 
-public class AuthServiceImpl {
+import com.bourbon_nook.users_api.entities.UserEntity;
+import com.bourbon_nook.users_api.repositories.UserRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository userRepository;
+
+    public AuthServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserEntity getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        String email = auth.getName();
+        return userRepository.findByEmail(email).orElse(null);
+    }
 }
