e0d3481bd06c125a73265c30e1f92eefecd243fe
Peter Eck <jpeckiii@gmail.com>
2026-07-30 10:15:29 -0500

    Add reviews and user generated code

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -15,4 +15,32 @@ export default defineConfig({
       },
     },
   },
+  usersApi: {
+    input: "http://localhost:8081/v3/api-docs",
+    output: {
+      mode: "single",
+      target: "src/api/generated/users-api.ts",
+      client: "axios",
+      override: {
+        mutator: {
+          path: "./src/api/axios-instance.ts",
+          name: "customInstance",
+        },
+      },
+    },
+  },
+  reviewsApi: {
+    input: "http://localhost:8084/v3/api-docs",
+    output: {
+      mode: "single",
+      target: "src/api/generated/reviews-api.ts",
+      client: "axios",
+      override: {
+        mutator: {
+          path: "./src/api/axios-instance.ts",
+          name: "customInstance",
+        },
+      },
+    },
+  },
 });
